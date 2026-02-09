9bb075016f8838aab6fa01270de5c93faee5b9e4
lausl11 <laura.laurindo@escola.pr.gov.br>
2026-02-09 08:27:23 -0300

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,9 @@
+# Solicita os 4 números ao usuário
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+num4 = float(input("Digite o quarto número: "))
+# Calcula a média dos 4 números
+media = (num1 + num2 + num3 + num4) / 4
+# Exibe o resultado com duas casas decimais
+print(f"A média é: {media:.2f}")
